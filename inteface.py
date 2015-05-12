03739e97367bd29eaa148e27ec8fed9017ba8134
Säbä <peterschwoaz@gmail.com>
2015-05-12 10:51:27 +0200

    Update inteface.py
    Nice

--- inteface.py
+++ inteface.py
@@ -1,17 +1,26 @@
 from tkinter import *
 
 root = Tk()
-#Menus.def
-def menutest():
-    print("Test")
+#Menus.def 
 
+m=IntVar()
+def menutest(menu):
+    if m.get()==0:
+        print("Test")
+        menu.entryconfigure(1, label="Auswahl2")
+        m.set(1)
+    elif m.get() ==1:
+        print("Test2")
+        +
+        menu.entryconfigure(1, label="Auswahl1")
+        m.set(0)
 
 #Temperatur.def
 def gettemp(event):
     karl=temregel.get()
     print(karl)
 
-def settemp():
+def settemp(event):
     frederick = settempein.get()
     peter= "sneaky"
     if frederick == peter:
@@ -26,33 +35,43 @@ def settemp():
 def manutemp(event):
     #print(arnold.get())
     if arnold.get()==0:
-        settemp.grid(row=3, column=0, sticky=E)
-        settempein.grid(row=3, column=1)
+        settempknopf.grid(row=4, column=0, sticky=E)
+        settempein.grid(row=4, column=1)
     elif arnold.get()==1:
-        settemp.grid_forget()
+        settempknopf.grid_forget()
         settempein.grid_forget()
 
 
 #Licht.def
-##def lichtan(event):
-##    if l==1:
-##        print("licht ist an")
-##        l=0
-##    elif l==0:
-##        print("licht ist aus")
-##        l=1
-    
+l=IntVar()
+def lichtan(event):
+    if l.get() == 0:
+        print("licht ist an")
+        l.set(1)
+    elif l.get() == 1:
+        print("licht ist aus")
+        l.set(0)
+     
 #Fenster.def
+df=IntVar()
 def dachf(event):
-    print("Dachfenster geöffnet")
+    if df.get() == 0:
+        print("Dachfenster geöffnet")
+        df.set(1)
+    elif df.get() == 1:
+        print("Dachfenster geschlossen")
+        df.set(0)
 
+#Tür.def
+
+        
 
 #Menu
 menu = Menu(root)
 root.config(menu=menu)
 Menus1=Menu(menu)
 menu.add_cascade(label="Menus1", menu=Menus1)
-Menus1.add_command(label="Auswahl1",command=menutest)
+Menus1.add_command(label="Auswahl1",command=lambda: menutest(Menus1))
 
 
 
@@ -61,6 +80,7 @@ Menus1.add_command(label="Auswahl1",command=menutest)
 Temp=Frame(root, cursor= "watch")
 Licht=Frame(root, cursor="spider")
 Fenster=Frame(root,cursor="pirate")
+Tür=Frame(root,cursor="star")
 
 
 
@@ -70,43 +90,69 @@ setmanu=Checkbutton(Temp, text="Manuelle Temperatureingabe?", variable = arnold)
 settempein = Entry(Temp)
 temregellable = Label(Temp, text = "Temperatur")
 temregel = Scale(Temp, from_ = -10, to= 40, orient = HORIZONTAL,length=200,tickinterval=10, cursor="watch")
-gettempknopf=Button(Temp, text="Hans Günther")
-settemp = Button(Temp, text="Karl Gustav", command= settemp)
+gettempknopf = Button(Temp, text="Hans Günther")
+settempknopf = Button(Temp, text="Karl Gustav")
 
 #Licht.Widgets
 lichtmain=Button(Licht, text="Lichthauptschalter")
 
 #Fenster.Widgets
-dachf=Button(Fenster,text="Dach")
+dachfenster=Button(Fenster,text="Dach")
 
+#Tür.Widgets
+
+#Label
+templabel= Label(Temp,text="Temperatur",font=("Arial",15,"bold","underline"))
+lichtlabel = Label(Licht, text="Licht",font=("Arial",15,"bold","underline"))
+fensterlabel = Label(Fenster, text="Fenster",font=("Arial",15,"bold","underline"))
+türlabel = Label(Tür, text="Türen",font=("Arial",15,"bold","underline"))
 
 
 
 #Temperatur.bind
 setmanu.bind("<Button-1>", manutemp)
 gettempknopf.bind("<Button-1>", gettemp)
-#settemp.bind("<Button-1>",settemp)
+settempknopf.bind("<Button-1>",settemp)
 
 #Licht.bind
 lichtmain.bind("<Button-1>",lichtan)
 
 #Fenster.bind
+dachfenster.bind("<Button-1>",dachf)
+
+#Tür.bind
 
 
+
+#Label.grid
+templabel.grid(row=0,columnspan=3)
+lichtlabel.grid(row=0,columnspan=3)
+fensterlabel.grid(row=0,columnspan=3)
+türlabel.grid(row=0,columnspan=3)
 
 
 
 
 #Temperatur.grid
-Temp.grid(row=0,column=0)
-setmanu.grid(row=2, column=1)
-temregellable.grid(row=0, column=0)
-temregel.grid(row=0, column=1)
-gettempknopf.grid(row=1, columnspan=2)
+Temp.grid(row=1,column=0,sticky=N)
+setmanu.grid(row=3, column=1)
+temregellable.grid(row=1, column=0)
+temregel.grid(row=1, column=1)
+gettempknopf.grid(row=2, columnspan=2)
 #Licht.grid
-Licht.grid(row=0,column=1)
-lichtmain.grid(row=0, column=0)
+Licht.grid(row=1,column=1,sticky=N)
+lichtmain.grid(row=1, column=0)
 #Fenster.grid
+Fenster.grid(row=2, column=0,sticky=N)
+dachfenster.grid(row=1,column=0)
+#Tür.grid
+Tür.grid(row=2,column=1,sticky=N)
 
+
+
+              
 
 root.mainloop()
+
+
+
